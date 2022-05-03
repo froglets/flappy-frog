258c94f8fb1035cb99e2e756c763776d513695aa
czoido <mrgalleta@gmail.com>
2022-05-03 07:38:02 +0200

    wip

--- conanfile.py
+++ conanfile.py
@@ -7,16 +7,16 @@ class FlappyFrogRecipe(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def configure(self):
-        # We can control the options of our dependencies based on current options
         if self.settings.os != "Linux":
             self.options["sdl_mixer"].nativemidi = False
 
     def requirements(self):
+        if self.settings.os != "iOS":
+            self.requires("cli11/2.1.2")
         self.requires("sdl/2.0.20")
         self.requires("sdl_image/2.0.5")
         self.requires("sdl_mixer/2.0.4")
         self.requires("box2d/2.4.1")
-        self.requires("cli11/2.1.2")
         self.requires("fmt/8.1.1")
         self.requires("tinkerforge-bindings/2.1.32")
 
