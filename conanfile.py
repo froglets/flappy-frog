6b63bcc48ba231749c16d3dd0612e7a714dce16d
czoido <mrgalleta@gmail.com>
2022-05-11 12:54:09 +0200

    use conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+
+
+class FlappyFrogRecipe(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def configure(self):
+        if self.settings.os != "Linux":
+            self.options["sdl_mixer"].nativemidi = False
+
+    def requirements(self):
+        self.requires("cli11/2.1.2")
+        self.requires("sdl/2.0.20")
+        self.requires("sdl_image/2.0.5")
+        self.requires("sdl_mixer/2.0.4")
+        self.requires("box2d/2.4.1")
+        self.requires("fmt/8.1.1")
+        self.requires("tinkerforge-bindings/2.1.32")
+
+        # Testing
+        self.requires("catch2/2.13.7")
