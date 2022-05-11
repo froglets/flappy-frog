e51105fb26c8b70a89686dc1397d587080188893
danimtb <danimanzaneque@gmail.com>
2022-05-11 12:21:48 +0200

    [feature] Create top body limit

--- src/World.cpp
+++ src/World.cpp
@@ -10,6 +10,15 @@ World::World() {
     b2PolygonShape groundBox;
     groundBox.SetAsBox(Game::WORLD_WIDTH/2.0, 0.5f);
     groundBody->CreateFixture(&groundBox, 0.0f);
+
+    // Create the top body
+    b2BodyDef topBodyDef;
+    b2Vec2 worldPos = b2Vec2(Game::WORLD_WIDTH/2.0, 6.0);
+    topBodyDef.position.Set(worldPos.x, worldPos.y);
+    b2Body* topBody = _world->CreateBody(&topBodyDef);
+    b2PolygonShape topBox;
+    topBox.SetAsBox(Game::WORLD_WIDTH/2.0, 0.5f);
+    topBody->CreateFixture(&topBox, 0.0f);
 }
 
 
