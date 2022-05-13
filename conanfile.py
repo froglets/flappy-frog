fdeb88a417096f460c302f5711a294e95e6f6fa6
danimtb <danimanzaneque@gmail.com>
2022-05-13 11:42:05 +0200

    try dependencies

--- conanfile.py
+++ conanfile.py
@@ -11,13 +11,13 @@ class FlappyFrom(ConanFile):
             self.options["sdl_mixer"].nativemidi = False
 
     def requirements(self):
-        self.requires("sdl/2.0.20")
-        self.requires("sdl_image/2.0.5")
+        #self.requires("sdl/2.0.20")
+        #self.requires("sdl_image/2.0.5")
         self.requires("sdl_mixer/2.0.4")
-        self.requires("box2d/2.4.1")
-        self.requires("cli11/2.1.2")
-        self.requires("fmt/8.1.1")
-        self.requires("tinkerforge-bindings/2.1.32")
+        #self.requires("box2d/2.4.1")
+        #self.requires("cli11/2.1.2")
+        #self.requires("fmt/8.1.1")
+        #self.requires("tinkerforge-bindings/2.1.32")
 
         # Testing
-        self.requires("catch2/2.13.7")
+        #self.requires("catch2/2.13.7")
