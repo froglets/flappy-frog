661efe8cd2f2fa9a8fd9000326ea0df987b1fc04
jgsogo <jgsogo@gmail.com>
2022-05-13 17:01:26 +0200

    disable everything

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,18 @@ class FlappyFrom(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def configure(self):
+        self.options["sdl_mixer"].flac = False
+        self.options["sdl_mixer"].mpg123 = False
+        self.options["sdl_mixer"].mad = False
+        self.options["sdl_mixer"].ogg = False
+        self.options["sdl_mixer"].opus = False
+        self.options["sdl_mixer"].mikmod = False
+        self.options["sdl_mixer"].modplug = False
+        self.options["sdl_mixer"].fluidsynth = False
         if self.settings.os != "Linux":
             self.options["sdl_mixer"].nativemidi = False
+        else:
+            self.options["sdl_mixer"].tinymidi = False
 
     def requirements(self):
         self.requires("sdl/2.0.20")
@@ -18,8 +28,6 @@ class FlappyFrom(ConanFile):
         self.requires("cli11/2.1.2")
         self.requires("fmt/8.1.1")
         self.requires("tinkerforge-bindings/2.1.32")
-
-        self.requires("libtool/2.4.6")
 
         # Testing
         self.requires("catch2/2.13.7")
